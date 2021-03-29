551daee065096b798817fdcf1561a21b75952b21
wellpinho <wellpinho@outlook.com>
2021-03-29 14:40:07 -0300

    Finalizado aula 01 nlw-4

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nlw-4
 Move It app created at Next Level Week #4 by Rocketseat - Trilha React
+
---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import { ExperienceBar } from './components/ExperiencBar'
+
+import './styles/global.css'
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
